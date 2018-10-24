26e75967efc86f717153f10cfcdc70867be7d4f0
Richard Tymotheus Purba <tepetrol@yahoo.com>
2018-10-24 07:46:08 +0200

    working on Bean

--- src/main/java/ethereumj/EthereumjBeans/EthterumBean.java
+++ src/main/java/ethereumj/EthereumjBeans/EthterumBean.java
@@ -1,8 +1,16 @@
 package ethereumj.EthereumjBeans;
 
-import org.etherum.facade.*
+import org.ethereum.core.block;
+import org.ethereum.facade.Ethereum;
+import org.ethereum.facade.EthereumFactory;
+
 
 public class EthterumBean {
 
-    private
+    private Ethereum ethereum;
+
+    public void start(){
+        this.ethereum = EthereumFactory.createEthereum();
+        this.ethereum.addListener();// make a listener.
+    }
 }
